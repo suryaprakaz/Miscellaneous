16c65dff691d7db5ac45cb4ca8f323a15c9b7fb9
suryaprakaz <suryaprakaz@hotmail.com>
2016-06-21 14:49:56 +0530

    Create mandragora_forest.cpp

---
+++ mandragora_forest.cpp
@@ -0,0 +1,52 @@
+/* MANDRAGORA FOREST */
+/*The evil forest is guarded by N vicious Mandragoras.
+  Each Mandragora has H health points.
+  Garnet and her pet begin their journey through the forest
+  
+  Initial Conditions:
+  HEALTH S=1
+  XP P=0
+  
+  To defeat ith Mandragora i, Garnet can perform either of the 
+  following actions:
+  
+  1. Garnet's pet can eat Mandragora i. S increases by 1
+     Mandragora i gets defeated. 
+  2. Garnet's pet battles Mandragora i. This increases P by
+     S x H experience points and defeats the Mandragora.
+  Each Mandragora can only be defeated once, and Garnet can
+  defeat the Mandragoras in any order. Each M's health points 
+  are given. Find the maximum number of XPs she can earn from
+  defeating all N Mandragoras
+  INPUT:
+    First Line: T ; Number of test cases
+    In each test case:
+        First Line: N; Number of Mandragoras
+        Second Line: N space separated integers that represent
+        the health of ith Mandragora.
+        
+   CONSTRAINTS:
+    1 <= T <= 10^5
+    1 <= N <= 10^5
+    1 <= Hi <= 10^7, where 1<= i <=N
+    
+    OUTPUT:
+    For each test case, print an integer denoting the max XPs
+    possible
+    
+*/
+  
+    
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}

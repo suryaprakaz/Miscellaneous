ea529dbfd7d7cdb1d6b3845afdc1045ecedcedbe
suryaprakaz <suryaprakaz@hotmail.com>
2017-09-22 16:39:51 +0530

    Create calc.h

---
+++ calc.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <conio.h>
+#include <ctype.h>
+#include <math.h>
+#include <vector>
+
+
+void Initialize();
+void calculate(std::string str);
+int Parse_string(std::string str);
+std::string addition(std::string num1, std::string num2);
+std::string subtraction(std::string num1, std::string num2);
+std::string multiplication(std::string num1, std::string num2);
+std::string division(std::string num1, std::string num2);
